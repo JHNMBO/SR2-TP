b1e0b7d2d7dc0d89eeffff819094fe8f3490803f
JHNMBO <145664794+JHNMBO@users.noreply.github.com>
2025-09-19 09:39:02 +0200

    Add files via upload

---
+++ 25-26-s1-tp-res-sr2/src/couche_transport.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "couche_transport.h"
+#include "services_reseau.h"
+#include "application.h"
+
+/* ************************************************************************** */
+/* *************** Fonctions utilitaires couche transport ******************* */
+/* ************************************************************************** */
+
+// RAJOUTER VOS FONCTIONS DANS CE FICHIER...
+
+int generer_controle(paquet_t *p) {
+    uint8_t somme = p->type^p->num_seq^p->lg_info;
+
+    for (int i = 0; i < p->lg_info; i++)
+    {
+        somme = somme^p->info[i];
+    }
+    return somme;
+}
+
+/*--------------------------------------*/
+/* Fonction d'inclusion dans la fenetre */
+/*--------------------------------------*/
+int dans_fenetre(unsigned int inf, unsigned int pointeur, int taille) {
+
+    unsigned int sup = (inf+taille-1) % SEQ_NUM_SIZE;
+
+    return
+        /* inf <= pointeur <= sup */
+        ( inf <= sup && pointeur >= inf && pointeur <= sup ) ||
+        /* sup < inf <= pointeur */
+        ( sup < inf && pointeur >= inf) ||
+        /* pointeur <= sup < inf */
+        ( sup < inf && pointeur <= sup);
+}
